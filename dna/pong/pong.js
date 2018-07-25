b90d486f77f73c7a0f72d8bfba1f04d1c9817fd8
Willem Olding <willemolding@gmail.com>
2018-07-25 15:13:53 -0600

    adds most required function skeletons

--- dna/pong/pong.js
+++ dna/pong/pong.js
@@ -1,31 +1,14 @@
 
+/*========================================
+=            public functions            =
+========================================*/
+
 function messageRef(payload) {
   var tickCount = typeof payload.tickCount === 'string' ? parseInt(payload.tickCount) : payload.tickCount;
   var vote = payload.vote;
   castVote(tickCount, vote);
   return true;
 }
-
-function getCurrentRef(tickCount) {
-  var agentList = getLinks(App.DNA.Hash, 'agent');
-  return agentList[tickCount % agentList.length].Hash;
-}
-
-function castVote(tickCount, vote) {
-  var ref = getCurrentRef(tickCount);
-  debug('Messaging: '+ref);
-  var message = {tickCount: tickCount, vote: vote}
-  send(ref, 
-    message, 
-    {Callback: { Function: "responseCallback", ID:''+tickCount}}
-  );
-}
-
-
-function responseCallback(response, id) {
-  debug("response of message tick "+id+": "+response);
-}
-
 
 function getResponses() {
   var result = query({
@@ -40,10 +23,69 @@ function getResponses() {
   return result;
 }
 
-function receive(from, message) {
-  debug('message received: ' +  JSON.stringify(message));
-  commit('voteRecord', message);
-  return true;
+
+function getCurrentState() {
+  var gameStates = getLinks(makeHash('anchor', 'stateAnchor'), 'gameState');
+  gameStates.sort(function (a, b) {
+    return b.tickCount - a.tickCount;
+  });
+  return gameStates[0];
+}
+
+/*=====  End of public functions  ======*/
+
+
+// a ref should call this function after they have received all votes
+// TODO: use a bundle to ensure all entries commit
+function commitNewState(tickCount) {
+  var votes = getReceivedVotes(tickCount);
+  var oldState = getCurrentState();
+  var newState = stateTransition(oldState, votes);
+  var stateAnchorHash = makeHash('anchor', 'stateAnchor');
+  var newStateHash = commit('gameState', {tickCount: tickCount, state: newState});
+  commit('link', {
+    Links: [{Base: stateAnchorHash, Link: newStateHash, Tag: 'gameState'}]
+  });
+}
+
+
+// takes an old state and some votes to return a new state
+// TODO: implement this
+function stateTransition(oldState, votes) {
+  return oldState;
+}
+
+
+function getCurrentRef(tickCount) {
+  var agentList = getLinks(App.DNA.Hash, 'agent');
+  return agentList[tickCount % agentList.length].Hash;
+}
+
+
+function castVote(tickCount, vote) {
+  var ref = getCurrentRef(tickCount);
+  debug('Messaging: '+ref);
+  var message = {tickCount: tickCount, vote: vote}
+  send(ref, 
+    message, 
+    { Callback: { Function: "responseCallback", ID:''+tickCount} }
+  );
+}
+
+
+// retrieve from local chain the votes sent to you at a specific tick count
+function getReceivedVotes(tickCount) {
+  var result = query({
+    Return: {
+      Entries: true
+    },
+    Constrain: {
+      EntryTypes: ["voteRecord"],
+      Contains: "{\"tickCount\": \""+tickCount+"\"}"
+    }
+  });
+  debug(result);
+  return result;
 }
 
 
@@ -51,11 +93,26 @@ function receive(from, message) {
 =            Callbacks            =
 =================================*/
 
+// commit a voteRecord to local chain when received via messaging
+// TODO: add validation
+function receive(from, message) {
+  debug('message received: ' +  JSON.stringify(message));
+  commit('voteRecord', message);
+  return true;
+}
+
+// called on the response of a message. For debug only at the moment
+function responseCallback(response, id) {
+  debug("response of message tick "+id+": "+response);
+}
+
 // link key hash to chain on genesis
+// also make sure the stateAnchor hash exists
 function genesis () {
-  commit('agentLink', {
+  commit('link', {
     Links: [{Base: App.DNA.Hash, Link: App.Key.Hash, Tag: 'agent'}]
   });
+  commit('anchor', 'stateAnchor');
   return true;
 }
 
